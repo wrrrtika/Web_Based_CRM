77c134e8f4218085e16aa06096cfd1a2fb8f73e4
JON HOUSE <J_HOUSE974@YAHOO.CA>
2014-10-28 14:37:14 -0400

    Continuing Progress

---
+++ Views/contact.rb
@@ -0,0 +1,10 @@
+contact.rb
+
+get "/contacts" do
+  @contacts = []
+  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+
+  erb :contacts
+end
