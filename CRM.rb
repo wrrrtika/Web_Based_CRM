262168bc46fccf3531a879c6b524c0e39fae60e0
JON HOUSE <J_HOUSE974@YAHOO.CA>
2014-10-28 14:43:13 -0400

    Updated CRM

--- CRM.rb
+++ CRM.rb
@@ -1,6 +1,17 @@
 require 'sinatra'
+require_relative 'contact'
+require_relative 'rolodex'
+
+$rolodex = Rolodex.new
 
 get '/' do 
 	@crm_app_name = "MY CRM"
 	erb :index
 end
+
+gets '/contacts' do
+	erb :contacts
+end
+
+get  '/contacts/new'
+	erb :
