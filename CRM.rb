c09ac51e6eae79c125bbfcfbc26ea69b35634221
JON HOUSE <J_HOUSE974@YAHOO.CA>
2014-10-28 15:45:56 -0400

    working copy of web-based CRM

--- CRM.rb
+++ CRM.rb
@@ -1,6 +1,7 @@
-require 'sinatra'
 require_relative 'contact'
 require_relative 'rolodex'
+require 'sinatra'
+
 
 $rolodex = Rolodex.new
 
@@ -9,9 +10,24 @@ get '/' do
 	erb :index
 end
 
-gets '/contacts' do
+get '/contacts' do
 	erb :contacts
 end
 
-get  '/contacts/new'
-	erb :
+get  '/contacts/new' do
+	erb :new_contact
+end
+
+get "/contacts" do
+  @contacts = []
+  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+	erb :contacts
+end
+
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  $rolodex.add_contact(new_contact)
+  redirect to('/contacts')
+end
